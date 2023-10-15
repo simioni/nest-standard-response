ee8f3a41b1aee35997e161453e03545c8ca24d8d
simioni <rsimioni@gmail.com>
2023-10-15 19:11:58 -0300

    Updated package exports to be more inclusive

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-import { StandardResponseModule } from "./standard-response/standard-response.module";
-export { StandardResponseModule };
+export * from "./standard-response/standard-response.module";
+export * from "./standard-response/standard-response.constants";
+
+export * from "./standard-response/interceptors/standard-response.interceptor";
+
+export * from "./standard-response/decorators/standard-response.decorator";
+export * from "./standard-response/decorators/standard-param.decorator";
+export * from "./standard-response/decorators/raw-response.decorator";
+
+export * from "./standard-response/dto/pagination-info.dto";
+export * from "./standard-response/dto/sorting-info.dto";
+export * from "./standard-response/dto/filtering-info.dto";
+export * from "./standard-response/dto/standard-response.dto";
+
+export * from "./standard-response/interfaces/standard-response-module-options.interface";
+export * from "./standard-response/interfaces/standard-response-options.interface";
